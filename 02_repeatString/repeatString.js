be4ef8d4f2ac6eac7c8b66551e0ab928df75349c
Jairo Lopez <jalopezing@gmail.com>
2024-06-17 14:11:16 -0500

    modified:   repeatString.js, repeatString.spec.js Second exercise completed.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    let finalWord ="";
+    if (times < 0){
+        finalWord = 'ERROR';
+    }
+    for (let i = 1; i<= times; i++){
+        finalWord += word;
+    }
+    return finalWord;
 
 };
 
