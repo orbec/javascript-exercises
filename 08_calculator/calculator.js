538127d9ffc4d37a7262dafb09d4d8c7fabdc26c
Jairo Lopez <jalopezing@gmail.com>
2024-06-18 12:55:50 -0500

    modified:   calculator.js, calculator.spec.js eigth exercise completed learned Array.reduce().

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(number1, number2) {
+  return number1+number2;
 };
 
-const subtract = function() {
-	
+const subtract = function(number1, number2) {
+	return number1-number2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((total,value) => total+= value,0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((total,value) => total *= value,1);
 };
 
-const power = function() {
-	
+const power = function(number1, number2) {
+	return number1**number2;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let result = 1;
+  if (number < 0) return -1;
+	while (number > 1){
+    result *= number;
+    number--;
+  }
+  return result;
 };
 
 // Do not edit below this line
