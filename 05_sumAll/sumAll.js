cc16d5e4f0c46ed9a0e46ed5beaf3ec5cbc45418
Jairo Lopez <jalopezing@gmail.com>
2024-06-18 11:37:58 -0500

    modified:   sumAll.js,   sumAll.spec.js Fifth exercise completed!

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,14 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    if (typeof number1 !== "number" || typeof number2 !== "number" || number1 < 0 || number2 < 0){
+        return "ERROR";
+    }
+    let n1 = number1 < number2 ? number1: number2;
+    let n2 = number1 > number2 ? number1: number2;
+    let result = 0;
+    for (n1; n1<=n2; n1++){
+        result +=n1;
+    }
+    return result;
 
 };
 
