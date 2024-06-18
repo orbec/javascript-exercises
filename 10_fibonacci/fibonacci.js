c6dbd2d49ab97f6f8f43c1037d1f30dfe595ec10
Jairo Lopez <jalopezing@gmail.com>
2024-06-18 14:18:05 -0500

    modified:   fibonacci.js,fibonacci.spec.js exercise done!

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number < 0 ) return "OOPS";
+    let result = 1;
+    let tempResult = 0;
+    for (let i = 1; i < +number; i++){
+        const final = tempResult + result;
+        tempResult = result;
+        result = final;
+
+    }
+    console.log(result);
+    return number == 0 ? tempResult : result;
 
 };
 
