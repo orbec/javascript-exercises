a006d8fd61a5203ee1937e41f8458ede8cfba24c
Jairo Lopez <jalopezing@gmail.com>
2024-06-18 16:14:36 -0500

    modified: findTheOldest.js, findTheOldest.spec.js Assignment finished! I learnt a lot.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.filter(
+        (person) => {
+            const maxAge = people.reduce(
+                (a, obj) =>{
+                    if (!obj.yearOfDeath) obj.yearOfDeath = new Date().getFullYear();
+                    obj.age = obj.yearOfDeath - obj.yearOfBirth;
+                    return Math.max(a,obj.age);
+                }
+            ,0);
+            return person.age === maxAge;
+        }
+    )
+    .at(0);
 };
 
 // Do not edit below this line
