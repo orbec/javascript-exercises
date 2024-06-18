1217a67e44883145728ab3e097411c99e813fe95
Jairo Lopez <jalopezing@gmail.com>
2024-06-18 13:46:13 -0500

    modified:   palindromes.js, palindromes.spec.js ninth excersise done!

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const cleanString = string
+        .toLowerCase()
+        .split("")
+        .filter(value => /[a-z0-9]/.test(value))
+        .join("");
+    const stringReversed = cleanString
+        .split("")
+        .toReversed()
+        .join("");
+    return cleanString === stringReversed;
 
 };
 
