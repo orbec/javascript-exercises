d9ca3d39112fd922e3a33db11474f0983fe5638a
Jairo Lopez <jalopezing@gmail.com>
2024-06-18 10:46:12 -0500

    mod:removeFromArray.js,removeFromArray.spec.js Fourth challenge completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
 const removeFromArray = function() {
+    let array = arguments[0];
+    for (let i = 1; i < arguments.length; i++){
+        array = array.filter((value) =>  value !== arguments[i]);
+    }
+    // solution provided
+    //   return array.filter(val => !args.includes(val))
+    return array;
 };
 
 // Do not edit below this line
